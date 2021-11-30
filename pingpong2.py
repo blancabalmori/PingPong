e72061a9e09cff1835f732c19e98357796f18c0b
blancabalmori <91721863+blancabalmori@users.noreply.github.com>
2021-11-30 14:32:20 +0100

    Tarea 7

--- pingpong2.py
+++ pingpong2.py
@@ -67,6 +67,19 @@ class RaquetaPong:
 
     def mover(self):
         self.y += self.dir_y
+        if self.y <= 0:
+            self.y = 0
+        if self.y + self.alto >= VENTANA_VERT:
+            self.y = VENTANA_VERT - self.alto
+    def golpear(self, pelota):
+        if (
+            pelota.x < self.x + self.ancho
+            and pelota.x > self.x
+            and pelota.y + pelota.alto > self.y
+            and pelota.y < self.y + self.alto
+        ):
+            pelota.dir_x = -pelota.dir_x
+            pelota.x = self.x + self.ancho
 
 def main():
     # Inicialización de Pygame
@@ -89,6 +102,8 @@ def main():
     while jugando:
         pelota.mover()
         pelota.rebotar()
+        raqueta_1.mover()
+        raqueta_1.golpear(pelota)
 
         ventana.fill(BLANCO)
         ventana.blit(pelota.imagen, (pelota.x, pelota.y))
@@ -98,6 +113,20 @@ def main():
         for event in pygame.event.get():
             if event.type == QUIT:
                 jugando = False
+            
+            # Detecta que se ha pulsado una tecla
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    raqueta_1.dir_y = -5
+                if event.key == pygame.K_s:
+                    raqueta_1.dir_y = 5
+
+            # Detecta que se ha soltado la tecla
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_w:
+                    raqueta_1.dir_y = 0
+                if event.key == pygame.K_s:
+                    raqueta_1.dir_y = 0
 
         pygame.display.flip()
         pygame.time.Clock().tick(FPS)
