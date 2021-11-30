02dfddd92dce750931043ab55deeec854fa77f5d
blancabalmori <91721863+blancabalmori@users.noreply.github.com>
2021-11-30 12:20:30 +0100

    Tarea 3

--- pingpong2.py
+++ pingpong2.py
@@ -33,6 +33,16 @@ class PelotaPong:
         self.x += self.dir_x
         self.y += self.dir_y
 
+    def rebotar(self):
+        if self.x <= 0:
+            self.dir_x = -self.dir_x
+        if self.x + self.ancho >= VENTANA_HORI:
+            self.dir_x = -self.dir_x
+        if self.y <= 0:
+            self.dir_y = -self.dir_y
+        if self.y + self.alto >= VENTANA_VERT:
+            self.dir_y = -self.dir_y
+
 
 def main():
     # Inicialización de Pygame
@@ -48,6 +58,7 @@ def main():
     jugando = True
     while jugando:
         pelota.mover()
+        pelota.rebotar()
 
         ventana.fill(BLANCO)
         ventana.blit(pelota.imagen, (pelota.x, pelota.y))
@@ -64,24 +75,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-def __init__(self, fichero_imagen):
-        # --- Atributos de la Clase ---
-
-        # Imagen de la Pelota
-        self.imagen = pygame.image.load(fichero_imagen).convert_alpha()
-
-        # Dimensiones de la Pelota
-        self.ancho, self.alto = self.imagen.get_size()
-
-        # Posición de la Pelota
-        self.x = VENTANA_HORI / 2 - self.ancho / 2
-        self.y = VENTANA_VERT / 2 - self.alto / 2
-
-        # Dirección de movimiento de la Pelota
-        self.dir_x = random.choice([-5, 5])
-        self.dir_y = random.choice([-5, 5])
-
-def mover(self):
-        self.x += self.dir_x
-        self.y += self.dir_y
